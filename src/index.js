1bddfb7f00354481e270a9e0509f41bee6e0de59
Stepan Ponomarev <step.ponomarev@gmail.com>
2022-05-31 03:39:56 +0300

    removed

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.createCacheStorage = void 0;
-var factory_1 = require("./storage/factory");
-exports.createCacheStorage = factory_1.createCacheStorage;
